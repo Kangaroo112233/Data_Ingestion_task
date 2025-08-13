a9c04d3f5c955ab030ac572d93b9768ae9f0ed22
venkatkrishnareddy <118088863+venkatkrishnareddy@users.noreply.github.com>
2025-08-13 09:09:30 -0500

    Update post_pro_8_13.py

--- code/python/post_pro_8_13.py
+++ code/python/post_pro_8_13.py
@@ -10,3 +10,15 @@ df5.loc[m, 'predicted_value'] = (
 df5.loc[m, 'ground_truth'] = (
     df5.loc[m, 'ground_truth'].astype(str).str.replace(r'[()]', '', regex=True)
 )
+
+
+m = df5['field'].eq('total_credits')
+
+pv = df5.loc[m, 'predicted_value'].map(to_money_decimal)
+gt = df5.loc[m, 'ground_truth'   ].map(to_money_decimal)
+
+# compare amounts (ignore sign); make sign matter by removing .abs()
+new_acc = (pv.abs() == gt.abs()).astype('Int64')
+
+# replace old accuracy with the new one for total_credits
+df5.loc[m, 'accuracy'] = new_acc
